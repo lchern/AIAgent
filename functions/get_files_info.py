68dfa718e77bf2c18db244a5a20a5de29bcf44b2
lchern <leonid.chern@gmail.com>
2026-07-10 17:54:45 +0400

    10.07.2026 added path validation and file listing; added 'calculator' app for further AI Agent training

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    body = ""
+    try:
+        abs_path = os.path.abspath(working_directory)
+    except Exception as e:
+        return (
+            f"Error: an unexpected error occurred while calling 'os.path.abspath': {e}"
+        )
+    try:
+        target_dir = os.path.normpath(os.path.join(abs_path, directory))
+    except Exception as e:
+        return (
+            f"Error: an unexpected error occurred while calling 'os.path.normpath': {e}"
+        )
+    try:
+        valid_target_dir = os.path.commonpath([abs_path, target_dir])
+    except Exception as e:
+        return f"\tError: an unexpected error occurred while calling 'os.path.commonpath': {e}"
+    if not valid_target_dir == abs_path:
+        body = f'\tError: Cannot list "{directory}" as it is outside the permitted working directory\n'
+    elif not os.path.isdir(target_dir):
+        body = f'Error: "{directory}" is not a directory\n'
+    else:
+        for item in os.listdir(target_dir):
+            body += f"  - {item}: file_size={os.path.getsize(os.path.join(target_dir, item))} bytes, is_dir={os.path.isdir(os.path.join(target_dir, item))}\n"
+    header = f'Result for "{directory}" directory:\n'
+    result = header.replace('"."', 'current') + body
+    return result
