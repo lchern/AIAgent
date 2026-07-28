9e1a66732e5b708bc2fa6d57901c164a3f496b5a
lchern <leonid.chern@gmail.com>
2026-07-28 13:00:02 +0400

    28.07.2026 added get_file_content and the related tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,33 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    file_content_string = ""
+    body = ""
+    try:
+        abs_path = os.path.abspath(working_directory)
+    except Exception as e:
+        return (
+            f"Error: an unexpected error occurred while calling 'os.path.abspath': {e}"
+        )
+    try:
+        target_path = os.path.normpath(os.path.join(abs_path, file_path))
+    except Exception as e:
+        return (
+            f"Error: an unexpected error occurred while calling 'os.path.normpath': {e}"
+        )
+    try:
+        valid_target_dir = os.path.commonpath([abs_path, target_path])
+    except Exception as e:
+        return f"\tError: an unexpected error occurred while calling 'os.path.commonpath': {e}"
+    if not valid_target_dir == abs_path:
+        body = f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(target_path):
+        body = f'Error: File not found or is not a regular file: "{file_path}"'
+    else:
+        with open(target_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+    result = body + file_content_string
+    return result
